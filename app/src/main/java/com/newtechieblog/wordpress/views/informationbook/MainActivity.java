727f26cdea1add9ddbb1515a92426051fcbd876c
Kelvin Ricafort <kelvs.r@outlook.com>
2023-10-27 15:54:41 +0800

    Creating Model Class

--- app/src/main/java/com/newtechieblog/wordpress/views/informationbook/MainActivity.java
+++ app/src/main/java/com/newtechieblog/wordpress/views/informationbook/MainActivity.java
@@ -1,14 +1,23 @@
 package com.newtechieblog.wordpress.views.informationbook;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+
     }
 }
