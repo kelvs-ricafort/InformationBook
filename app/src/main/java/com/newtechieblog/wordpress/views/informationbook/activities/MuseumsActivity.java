b3dcdc7138435f618df9e03577e5dce541088c28
Kelvin Ricafort <kelvs.r@outlook.com>
2023-10-30 14:22:00 +0800

    Creating the Activities

---
+++ app/src/main/java/com/newtechieblog/wordpress/views/informationbook/activities/MuseumsActivity.java
@@ -0,0 +1,16 @@
+package com.newtechieblog.wordpress.views.informationbook.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.newtechieblog.wordpress.views.informationbook.R;
+
+public class MuseumsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_museums);
+    }
+}
--- app/src/main/java/com/newtechieblog/wordpress/views/informationbook/adapters/AdapterClass.java
+++ app/src/main/java/com/newtechieblog/wordpress/views/informationbook/adapters/AdapterClass.java
@@ -1,15 +1,21 @@
 package com.newtechieblog.wordpress.views.informationbook.adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.newtechieblog.wordpress.views.informationbook.R;
+import com.newtechieblog.wordpress.views.informationbook.activities.CountriesActivity;
+import com.newtechieblog.wordpress.views.informationbook.activities.LeadersActivity;
+import com.newtechieblog.wordpress.views.informationbook.activities.MuseumsActivity;
+import com.newtechieblog.wordpress.views.informationbook.activities.WondersActivity;
 import com.newtechieblog.wordpress.views.informationbook.models.ModelClass;
 
 import java.util.ArrayList;
@@ -26,17 +32,43 @@ public class AdapterClass extends RecyclerView.Adapter<AdapterClass.CardViewHold
     @Override
     public CardViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_design, parent, false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_design, parent,false);
 
         return new CardViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(CardViewHolder holder, int position) {
+    public void onBindViewHolder(CardViewHolder holder, final int position) {
         ModelClass model = modelList.get(position);
         holder.textViewSplash.setText(model.getCategoryName());
         holder.imageViewSplash.setImageResource(context.getResources()
                 .getIdentifier(model.getImageName(), "drawable", context.getPackageName()));
+
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (position == 0) {
+                    Intent intent = new Intent(context, CountriesActivity.class);
+                    context.startActivity(intent);
+                }
+
+                if (position == 1) {
+                    Intent intent = new Intent(context, LeadersActivity.class);
+                    context.startActivity(intent);
+                }
+
+                if (position == 2) {
+                    Intent intent = new Intent(context, MuseumsActivity.class);
+                    context.startActivity(intent);
+                }
+
+                if (position == 3) {
+                    Intent intent = new Intent(context, WondersActivity.class);
+                    context.startActivity(intent);
+                }
+            }
+        });
     }
 
     @Override
@@ -48,11 +80,14 @@ public class AdapterClass extends RecyclerView.Adapter<AdapterClass.CardViewHold
 
         private ImageView imageViewSplash;
         private TextView textViewSplash;
+        private CardView cardView;
+
         public CardViewHolder(View itemView) {
             super(itemView);
 
             imageViewSplash = itemView.findViewById(R.id.imageViewSplash);
             textViewSplash = itemView.findViewById(R.id.textViewSplash);
+            cardView = itemView.findViewById(R.id.cardView);
 
         }
     }
